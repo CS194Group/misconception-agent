800a069f9ff1d80a76a4e936f7affa523d853dcf
Amaan Ansari <amaan.ansari@students.uni-mannheim.de>
2024-11-29 10:28:52 -0800

    <update> fixed training and evaluating; add LanguageModel wrapper to allow usage calculation

--- main.py
+++ main.py
@@ -5,73 +5,69 @@ TODO:
 3. Trying more advanced structure.
 '''
 
-import os
 import pathlib
+import time
+from typing import Literal
 
 import dspy
-
-from dotenv import load_dotenv
+from colorama import init
 from dspy.evaluate import Evaluate
-from colorama import Fore, Style, init
 from dspy.teleprompt import BootstrapFewShot
 from sklearn.model_selection import train_test_split
 
 from src.agents import Agent
-from src.evaluation import evaluate_answers
+from src.dataloader import DataManager
+from src.evaluation import EvaluationManager
 from src.predict_model import ExchangeOfThought
-from src.dataloader import load_data, load_misconceptions, DataManager
+from src.util import LanguageModel
 
 # Initialize colorama
 init(autoreset=True)
 
-# Load environment variables
-load_dotenv()
-
 # CONSTANTS
-API = 'lambda'
-MAX_TOKEN = 250
-DEBUG = True
-SEED = 42
+DEBUG: bool = True
+SEED: int = 42
+API: Literal['lambda', 'openai'] = 'lambda'
+MAX_TOKEN: int = 100
 
-lm = None
-if API == 'lambda':
-    lm = dspy.LM(f"openai/{os.getenv('API_MODEL')}", max_tokens=MAX_TOKEN, api_key=os.getenv("OPENAI_API_KEY"),
-            api_base=os.getenv("OPENAI_API_BASE"))
-elif API == 'openai':
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    if not OPENAI_API_KEY:
-        raise EnvironmentError(
-            "OPENAI_API_KEY not found in environment variables.")
-    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-    lm = dspy.LM('openai/gpt-4o-mini', max_tokens=MAX_TOKEN)
-
-assert lm is not None
-dspy.configure(lm=lm)
+lm_wrapper = LanguageModel(max_tokens=MAX_TOKEN, service=API)
+dspy.configure(lm=lm_wrapper.lm)
 
 if __name__ == "__main__":
-    # Misconception mapping
-    misconceptions = load_misconceptions('data/misconception_mapping.csv')
-
     # Load training and test sets
+    start = time.time()
     examples = DataManager.get_examples(pathlib.Path("data"), debug=DEBUG)
-    train_data, test_data = train_test_split(examples, test_size=0.2, random_state=SEED)
+
+    # Split in 80% validation as this is what is suggested here https://dspy.ai/learn/optimization/overview/
+    train_data, val_data = train_test_split(examples, test_size=0.8, random_state=SEED)
 
     # Set up Agents
     agent_a = Agent(name="Agent A")
     agent_b = Agent(name="Agent B")
     agent_c = Agent(name="Agent C")
 
-    # evaluate
-    evaluate_program = Evaluate(devset=test_data, metric=evaluate_answers,
-                                num_threads=8, display_progress=True, display_table=10)
     predict = ExchangeOfThought(
-        agent_a, agent_b, agent_c, rounds=3, mode="Report")
-    eval1 = evaluate_program(predict)
+        agent_a, agent_b, agent_c, rounds=1, mode="Report")
+    evaluation_metric = EvaluationManager.metric
 
     # compile
-    teleprompter = BootstrapFewShot(
-        metric=evaluate_answers, max_labeled_demos=1)
+
+    # teleprompter = BootstrapFewShot(metric=evaluation_metric, max_labeled_demos=2)
+    # compiled_predictor = teleprompter.compile(predict, trainset=train_data)
+    teleprompter = dspy.MIPROv2(metric=evaluation_metric, auto='light', num_threads=50)
     compiled_predictor = teleprompter.compile(predict, trainset=train_data)
+    compiled_predictor.save("models" / pathlib.Path('compiled_model.dspy'))
+
+    # evaluate
+    evaluate_program = Evaluate(devset=val_data, metric=evaluation_metric,
+                                num_threads=50, display_progress=True, display_table=10)
+
+    eval_result = evaluate_program(predict)
+    end = time.time()
+    usage = lm_wrapper.get_usage()
+    print(eval_result)
+    print(f"Usage cost (in cents) about {usage[2]}, Input Tokens: {usage[0]}, Output Tokens {usage[1]}" )
+    print("Time taken (in seconds)", end - start)
 
     # evaluate again
     # eval_compiled = evaluate_program(compiled_predictor)
