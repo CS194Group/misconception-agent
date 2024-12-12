de78f8d4c5c77ddafd7ea6034414bd8bc5616f1e
Amaan Ansari <amaan.ansari@students.uni-mannheim.de>
2024-12-12 15:36:13 -0800

    <add> new models

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ TODO:
 2. Figuring out how to handle the inconsistency between training data and prediction results.
 3. Trying more advanced structure.
 '''
-
+import os
+import certifi
+os.environ['SSL_CERT_FILE'] = certifi.where()
 import pathlib
 import time
+import pdb
 from typing import Literal
 
 import dspy
@@ -15,7 +18,7 @@ from dspy.evaluate import Evaluate
 from dspy.teleprompt import BootstrapFewShot
 from sklearn.model_selection import train_test_split
 
-from src.agents import Agent
+from src.agents import Agent, AdvancedAgent
 from src.dataloader import DataManager
 from src.evaluation import EvaluationManager
 from src.predict_model import ExchangeOfThought
@@ -27,7 +30,7 @@ init(autoreset=True)
 
 # CONSTANTS
 DEBUG: bool = True
-SEED: int = 42
+SEED: int = 39
 API: Literal['lambda', 'openai'] = 'lambda'
 MAX_TOKEN: int = 100
 
@@ -44,17 +47,22 @@ if __name__ == "__main__":
     train_data, val_data = train_test_split(examples, test_size=0.8, random_state=SEED)
 
     # Set up Agents
+    # agent_a = Agent(name="Agent A" , persona_promt=Persona.AGENT_A_new)
+    # agent_b = Agent(name="Agent B" , persona_promt=Persona.AGENT_B_new)
+    # agent_c = Agent(name="Agent C" , persona_promt=Persona.AGENT_C_new)
+    # agent_d = Agent(name="Agent d" , persona_promt=Persona.AGENT_D_new)
+    # agent_e = Agent(name="Agent e" , persona_promt=Persona.AGENT_E_new)
+    
+    agent_a = AdvancedAgent(name="Agent A" , persona_promt=Persona.AGENT_A_new)
+    agent_b = AdvancedAgent(name="Agent B" , persona_promt=Persona.AGENT_B_new)
+    agent_c = AdvancedAgent(name="Agent C" , persona_promt=Persona.AGENT_C_new)
+    agent_d = AdvancedAgent(name="Agent D" , persona_promt=Persona.AGENT_D_new)
+    agent_e = AdvancedAgent(name="Agent E" , persona_promt=Persona.AGENT_E_new)
 
-    # agent_a = Agent(name="Agent A" )#, persona_promt=Persona.AGENT_A_new)
-    # agent_b = Agent(name="Agent B" )#, persona_promt=Persona.AGENT_B_new)
-    # agent_c = Agent(name="Agent C" )#, persona_promt=Persona.AGENT_C_new)
-
-    agent_a = Agent(name="Agent A" , persona_promt=Persona.AGENT_A_new)
-    agent_b = Agent(name="Agent B" , persona_promt=Persona.AGENT_B_new)
-    agent_c = Agent(name="Agent C" , persona_promt=Persona.AGENT_C_new)
+    # print(agent_a(train_data[3]['QuestionText'], train_data[3]['AnswerText'], train_data[3]['ConstructName'], train_data[3]['SubjectName'], train_data[3]['CorrectAnswer']))
 
     predict = ExchangeOfThought(
-        agent_a, agent_b, agent_c, rounds=1, mode="Report")
+        agent_a, agent_b, agent_c, agent_d, agent_e, rounds=1, mode="Report")
     # predict = Agent(name="Single Agent" )
     evaluation_metric = EvaluationManager().metric_vector_search
 
@@ -65,16 +73,12 @@ if __name__ == "__main__":
 
     # evaluate
     evaluate_program = Evaluate(devset=val_data, metric=evaluation_metric,
-                                num_threads=24, display_progress=True, display_table=10)
+                                num_threads=1, display_progress=True, display_table=10)
 
-    eval_result = evaluate_program(predict)
+    eval_result = evaluate_program(compiled_predictor)
     end = time.time()
     usage = lm_wrapper.get_usage()
+    # pdb.set_trace()
     print(eval_result)
     print(f"Usage cost (in cents) about {usage[2]}, Input Tokens: {usage[0]}, Output Tokens {usage[1]}" )
     print("Time taken (in seconds)", end - start)
-
-    # evaluate again
-    # eval_compiled = evaluate_program(compiled_predictor)
-    # compiled_predictor.save('./compiled_model.pkl')
-    # print(eval_compiled)
--- src/predict_model.py
+++ src/predict_model.py
@@ -1,4 +1,5 @@
 import time
+import pdb
 from typing import Literal
 
 import dspy
@@ -23,15 +24,23 @@ class SharedMemoryPool:
 
 
 class ExchangeOfThought(dspy.Module):
-    def __init__(self, agent_a, agent_b, agent_c, rounds: int = 1, mode: Literal["Debate", "Report", "Memory", "Relay"] = "Report"):
+    # def __init__(self, agent_a, agent_b, agent_c, rounds: int = 1, mode: Literal["Debate", "Report", "Memory", "Relay"] = "Report"):
+    def __init__(self, agent_a, agent_b, agent_c, agent_d=None, agent_e=None, rounds: int = 1, mode: Literal["Debate", "Report", "Memory", "Relay"] = "Report"):
         super().__init__()
         self.agent_a = agent_a
         self.agent_b = agent_b
         self.agent_c = agent_c
+        self.agent_d = agent_d
+        self.agent_e = agent_e
         self.memory_pool = SharedMemoryPool()
         self.rounds = rounds
         self.mode = mode
-        assert self.mode == "Report"
+        # assert self.mode == "Report"
+
+        # # below is the multi init
+        # self.expert_group_a = expert_a
+        # self.expert_group_b = expert_b
+        # self.expert_group_c = expert_c
 
     def forward(self, QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer):
         #time.sleep(0.5)
@@ -43,12 +52,19 @@ class ExchangeOfThought(dspy.Module):
             return self._memory_mode(QuestionText)
         elif self.mode == "Relay":
             return self._relay_mode(QuestionText)
+        elif self.mode == "multi":
+            return self._multi_mode(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer)
+        elif self.mode == "multi_4":
+            return self._multi4_mode(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer)
+        elif self.mode == "bigram":
+            return self._bigram_mode(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer)
         else:
             raise ValueError(f"Invalid mode: {self.mode}")
 
     def _report_mode(self, QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer):
         # Step 1: A initiates thought
         thought_a = self.agent_a(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer)
+        # pdb.set_trace()
 
         # Note this for-loop does not keep history of previous rounds, but it includes the chain of toughts if the agents
         for _ in range(self.rounds):
@@ -126,5 +142,43 @@ class ExchangeOfThought(dspy.Module):
         thought_a.question = question
 
         return thought_a
+    
+    def _multi4_mode(self, QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer):
+        # Step 1: A initiates thought
+        thought_a = self.agent_a(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer)
+
+
+        for _ in range(self.rounds):
+
+            agent_a_history = f"Agent A concludes: ({str(thought_a)})"
+            thought_b = self.agent_b(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer, context=agent_a_history)
+            thought_c = self.agent_c(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer, context=agent_a_history)
+
+            prompt = f"For this question's misconception, student b's ideas is \n{thought_b}\nstudent c's ideas is \n{thought_c}\n"
+
+            thought_d = self.agent_d.forward(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer, context=prompt)
+
+            thought_a = self.agent_a.forward(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer, context=thought_d)
+
+        return thought_a
+    
+    def _bigram_mode(self, QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer):
+        # Step 1: A initiates thought
+        thought_a = self.agent_a(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer)
+
+        for _ in range(self.rounds):
+
+            thought_b = self.agent_b.forward(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer, context=thought_a)
+            thought_c = self.agent_c.forward(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer, context=thought_a)
+
+            thought_b = self.agent_b.forward(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer, context=thought_c)
+            thought_c = self.agent_c.forward(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer, context=thought_b)
+
+            prompt = f"For this question's misconception, student b's ideas is \n{thought_b}\nstudent c's ideas is \n{thought_c}\n"
+
+            thought_a = self.agent_a.forward(QuestionText, AnswerText, ConstructName, SubjectName, CorrectAnswer, context=prompt)
+
+        return thought_a
+
 
 #########################################################################################################################
